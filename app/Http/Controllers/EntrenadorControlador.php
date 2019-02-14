1223dc19de4b736ad32baa0e4862039dc1ef6295
LoquilloDuvan <programmerloquillo@gmail.com>
2019-02-13 21:59:02 -0500

    Creacion de la vista crear

--- app/Http/Controllers/EntrenadorControlador.php
+++ app/Http/Controllers/EntrenadorControlador.php
@@ -11,8 +11,7 @@ class EntrenadorControlador extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         return "hola controlador resource";
     }
 
@@ -21,9 +20,8 @@ class EntrenadorControlador extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create() {
+        return view('entrenadores.crear');
     }
 
     /**
---
+++ resources/views/entrenadores/crear.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Crear</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="form-group">
+			<label for="">Nombre</label>
+			<input type="text" class="form-control">
+			<buttom class="btn btn-primary" type="submit">Enviar</buttom>
+		</div>
+	</div>
+</body>
+</html>
