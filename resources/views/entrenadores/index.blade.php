951e516a526f059537d7456623cc7b8c20e11003
LoquilloDuvan <programmerloquillo@gmail.com>
2019-02-14 21:26:12 -0500

    Arreglando la vista

--- resources/views/entrenadores/index.blade.php
+++ resources/views/entrenadores/index.blade.php
@@ -5,9 +5,9 @@
 @section('contenido')
 	<div class="row">
 		@foreach($entrenadores as $entrenador)
-		<div class="col-sm">
-			<div class="card text-center" style="width: 18rem;">
-				<img src="imagenes/{{$entrenador->avatar}}" alt="" class="card-img-top">
+		<div  class="col-sm">
+			<div class="card text-center" style="width: 18rem; margin-top: 70px;">
+				<img style="height: 130px; width: 130px; background-color: #EFEFEF; margin: 20px;" src="imagenes/{{$entrenador->avatar}}" alt="" class="card-img-top rounded-circle mx-auto d-block">
 				<div class="card-body">
 					<h5 class="card-title">{{$entrenador->nombre}}</h5>
 					<p class="card-text">{{$entrenador->descripcion}}</p>
